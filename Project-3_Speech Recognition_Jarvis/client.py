00d2561e1ee873a43c6128a8dd769f666ef123dd
Bigyanshu <142771738+Bigyanshu@users.noreply.github.com>
2024-11-21 23:18:52 +0530

    Update client.py
    Removes API keys

--- Project-3_Speech Recognition_Jarvis/client.py
+++ Project-3_Speech Recognition_Jarvis/client.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
 client = OpenAI(
-    api_keys = "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"
+    api_keys = "Enter Your API keys"
 )
 
 completion = client.chat.completions.create(
